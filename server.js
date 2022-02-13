6627de43a81a9c5682e6bbb93f966bb744009ada
Antonio Seguil <antonioseguil@gmail.com>
2022-02-13 12:39:32 -0500

    Final del proyecto

--- server.js
+++ server.js
@@ -1,25 +1,54 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/shorturl', (req,res,next) =>{
-  console.log("middeware");
-}, (req, res) => {
-  res.send("prueba");
+const urlValid = [
+  { original_url: "https://freeCodeCamp.org", short_url: 1 },
+  { original_url: "https://google.com.pe", short_url: 2 },
+];
+
+function seachrUrl(url = "") {
+  return urlValid.findIndex((findurl) => findurl.original_url == url);
+}
+
+app.post(
+  "/api/shorturl",
+  (req, res, next) => {
+    //validamos que exista la url
+    let urlParam = req.body.url;
+    let searchUrl = seachrUrl(urlParam);
+    if (searchUrl >= 0) {
+      next();
+    } else {
+      res.json({ error: "invalid url" });
+    }
+  },
+  (req, res) => {
+    let url = req.body.url;
+    let objurl = urlValid[seachrUrl(url)];
+    res.json(objurl);
+  }
+);
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  let obj = urlValid.find((urlS) => urlS.short_url == req.params.short_url);
+  res.redirect(obj.original_url);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
