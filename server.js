067464b68d451ab7b7f978012292ac754ec46b03
Antonio SeguilR <34606236+antonioseguil@users.noreply.github.com>
2022-02-13 22:34:57 +0000

    ajuste

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
@@ -24,33 +24,36 @@ app.post(
   "/api/shorturl",
   (req, res, next) => {
     //validamos que exista la url
-    let urlParam = req.body.url;
+    let urlParam = req.body.url.replace('https://','');
+    console.log("url enviada");
+    console.log(urlParam);
     dns.lookup(urlParam, (err, address, family) => {
       if (err) {
-        res.json(err);
         res.json({ error: "invalid url" });
       } else {
+        console.log("si paso");
         next();
       }
     });
   },
   (req, res) => {
+    console.log("entro");
     let url = req.body.url;
     let obj = {
       original_url: url, short_url: (urlValid.length + 1)
     }
     urlValid.push(obj);
+    console.log("viendo cadena");
+    console.log(urlValid);
     res.json(obj);
   }
 );
 
 app.get("/api/shorturl/:short_url", (req, res) => {
   let obj = urlValid.find((urlS) => urlS.short_url == req.params.short_url);
-  console.log("Busqueda:");
-  console.log(obj);
-  res.redirect('https://' + obj.original_url);
+  res.redirect(obj.original_url);
 });
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
